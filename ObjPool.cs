86a7f19b9147271a2531b4267dfbdd2c03d92ec2
Redcatsan <80650420+Redcatsan@users.noreply.github.com>
2021-10-17 14:01:19 +0900

    Update ObjPool.cs

--- ObjPool.cs
+++ ObjPool.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class ObjPool : MonoBehaviour
 {
-    public static Dictionary<string,Stack<object>> ObjPoolDic = new Dictionary<string,Stack<object>>();
-    public static List<object> EnableObjList = new List<object>();
+    public static Dictionary<string,Stack<GameGameObject>> ObjPoolDic = new Dictionary<string,Stack<GameObject>>();
+    public static List<GameObject> EnableObjList = new List<GameObject>();
 
-    public static void Register(object target,int count){//追加・登録
+    public static void Register(GameObject target,int count){//追加・登録
         if(!ObjPoolDic.ContainsKey(target.name)){
-            ObjPoolDic.Add(target.name,new Stack<object>());
+            ObjPoolDic.Add(target.name,new Stack<GameObject>());
         }
         for(int a = 0;a<count;a++){
-            object pObj = Instantiate(target);
+            GameObject pObj = Instantiate(target);
             pObj.SetActive(false);
             ObjPoolDic[target.name].Push(pObj);
         }
     }
 
-    public static object Create(string name,Vector3? pos=  null,Quaternion? rot = null){//生成
+    public static GameObject Create(string name,Vector3? pos=  null,Quaternion? rot = null){//生成
         if(!ObjPoolDic.ContainsKey(name)){
             print("You must need Register() to use Create() method");
         }
-        object returnobj = ObjPoolDic[name].Pop();
+        GameObject returnobj = ObjPoolDic[name].Pop();
         returnobj.SetActive(true);
         EnableObjList.Add(returnobj);
         if(pos != null){
@@ -34,7 +34,7 @@ public class ObjPool : MonoBehaviour
         return returnobj;
     }
 
-    public static void Sleep(object target){//使い終わった後の処理
+    public static void Sleep(GameObject target){//使い終わった後の処理
         target.SetActive(false);
         EnableObjList.Remove(target);
         ObjPoolDic[target.name].Push(target);
